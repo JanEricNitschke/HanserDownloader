99dc2bc548c56698226ef7b2e265e7a871eea7f8
Jan-Eric Nitschke <jan-eric@nitschke.tk>
2022-12-26 09:38:10 +0100

    Changed structure to match desired webpages. Added proper naming of downloaded chapters.

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # HanserDownloader
 
 Script to collect and download all pdf files for a book on the Hanser Verlag website.
+
+Install dependencies with:
+```bash
+pip install -r requirements.txt
+```
--- download_pdfs.py
+++ download_pdfs.py
@@ -1,7 +1,7 @@
 """Script to download all pdfs for a book from the hanser verlag
 
 Typical usage example:
-    python download_pdfs.py --dir "D:\\Downloads\\Hanser\\" --url "https://downloads.hanser.de/index.asp?isbn=978-3-446-43085-3&nav_id=763475834&nav_page=2"
+    python download_pdfs.py --dir "D:\\Downloads\\Hanser\\" --url "https://www.hanser-elibrary.com/isbn/9783446453968"
 """
 #!/usr/bin/env python
 
@@ -11,7 +11,7 @@ import logging
 import argparse
 import requests
 from bs4 import BeautifulSoup
-
+ 
 
 def main(args):
     """Downloads demos from hltv.org"""
@@ -55,6 +55,8 @@ def main(args):
             datefmt="%Y-%m-%d %H:%M:%S",
         )
 
+
+    logging.info("Running with output directory: %s and URL: %s", options.dir, options.url)
     # URL from which pdfs to be downloaded
     url = options.url
 
@@ -64,30 +66,53 @@ def main(args):
     # Parse text obtained
     soup = BeautifulSoup(response.text, "html.parser")
 
-    # Find all hyperlinks present on webpage
-    links = soup.find_all("a")
+    # For the Hanser Verlag the Chapter titles can be found like this (26.12.2022 9:07):
+    # <h5>Die Konkubinenwirtschaft</h5>
+    # document.querySelector("#\\31 0\\.3139\\/9783446418240\\.fm > h5")
+    # /html/body/div[1]/div/main/div[2]/div[2]/div/div[1]/div[3]/div[2]/div[1]/div/div/div[2]/div[1]/a/h5
+    # Get all h5 headings that do not have a class and get their inner text:
+    # Should result in a list like this:
+    # ['Die Konkubinenwirtschaft', 'Nicht lange Fackeln', 'Auf und Nieder immer wieder', 'Immer flüssig', 'Wahaha', 'Stets starkes Signal', 'Mit Mann und Maus',
+    # 'QQ ohne IQ', 'Akku leer', 'Bohren für China', 'Über den Wolken', 'Haier und Higher', 'Störsender und Chinesin',
+    # 'Hochstapeln leicht gemacht', 'Meisterzeit', 'Literaturverzeichnis']
+    # Order should be the same as that of the links
+    names = list(map(lambda h5: h5.text.strip(), soup.find_all("h5", {"class": ""})))
 
+    # Find all hyperlinks present on webpage
+    # link_url = link.get("href") # /doi/epdf/10.3139/9783446456013.012
+    # link_url = "https://www.hanser-elibrary.com" + link_url # https://www.hanser-elibrary.com/doi/epdf/10.3139/9783446456013.012
+    # link_url = link_url.replace("epdf", "pdf") # https://www.hanser-elibrary.com/doi/pdf/10.3139/9783446456013.012
+    # For the Hanser Verlag the pdf link elements look like this (26.12.2022 8:48):
+    # <a title="PDF" class="btn btn--light-bg" href="/doi/epdf/10.3139/9783446456013.012"><i aria-hidden="true" class="icon-PDF inline-icon"></i><span class="text">PDF</span></a>
+    # The full xpath/jspath of the elements are (26.12.2022 8:48):
+    # /html/body/div[1]/div/main/div[2]/div[2]/div/div[1]/div[3]/div[2]/div[14]/div/div/div[3]/ul/li[2]/a
+    # document.querySelector("#pb-page-content > div > main > div.container.shift-up-content > div.page__content.padding-wrapper.table-of-content-page > div > div.col-lg-8.col-md-8 > 
+    # div.toc-container > div.table-of-content > div:nth-child(1) > div > div > div.issue-item__footer > ul > li:nth-child(2) > a")
+    links = list(map(lambda link: ("https://www.hanser-elibrary.com" + link.get("href")).replace("epdf","pdf"), filter(lambda link: link.get("title") == "PDF", soup.find_all("a"))))
+    
+    if len(names) != len(links):
+        raise AssertionError("Could not find an equal number of chapter names and link!")
     i = 0
 
     # From all links check for pdf link and
     # if present download file
-    for link in links:
-        if any(
-            pdf_identifier in link.get("href", [])
-            for pdf_identifier in [".pdf", "/pdf"]
-        ):
-            i += 1
-            logging.info("Downloading file: %s", i)
+    for link_url in links:
+        i += 1
+        logging.info("Downloading file: %s", i)
 
-            # Get response object for link
-            response = requests.get(link.get("href"))
+        logging.info("Downloading pdf from link: %s", link_url)
+        
+        # Get response object for link
+        response = requests.get(link_url)
 
-            # Write content in pdf file
-            filename = os.path.join(options.dir, "pdf" + str(i) + ".pdf")
-            logging.info("Saving file to %s", filename)
-            with open(filename, "wb") as pdf:
-                pdf.write(response.content)
-            logging.info("File %s downloaded", i)
+        # Write content in pdf file
+        filename = os.path.join(options.dir, f"Chapter{i}_{names[i-1]}.pdf")
+        logging.info("Saving file to %s", filename)
+        # Add try except for invalid file names
+        # OSError: [Errno 22] Invalid argument: 'D:\\Downloads\\Hanser\\Chapter7_EIN GEGENMODELL IM WERDEN?.pdf'
+        with open(filename, "wb") as pdf:
+            pdf.write(response.content)
+        logging.info("File %s downloaded", i)
 
     logging.info("All PDF files downloaded")
 
